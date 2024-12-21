26c90db1c7d580e86d9a20982819e532afc89a9a
Nguyễn Huy Hoàng <168755579+nguyen-huy-hoang-1708@users.noreply.github.com>
2024-12-21 14:58:46 +0700

    GUIProject

--- src/module-info.java
+++ src/module-info.java
@@ -1,12 +1,14 @@
-module AimsProject {
-    requires java.desktop; // For AWT and Swing
-    requires javafx.controls; // For JavaFX UI controls
-    requires javafx.fxml; // For FXML files
-    requires javafx.graphics; // For JavaFX graphics
-    requires javafx.swing; // For using JFXPanel
-
-    opens hust.soict.dsai.aims.screen.controller to javafx.fxml, javafx.graphics; // Reflective access for FXML loader
-    opens hust.soict.dsai.aims.screen.view to javafx.fxml; // If needed for FXML files in the "view" package
-
-    exports hust.soict.dsai.aims.media; // Continue exporting media-related packages if needed
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GUIProject {
+	requires java.desktop;
+	requires javafx.controls;
+    requires javafx.fxml;
+    
+    exports hust.soict.dsai.javafx;
+    opens hust.soict.dsai.javafx to javafx.fxml;
 }
